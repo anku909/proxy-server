829a28b4a438cca451b3d3c2a922107869e00ed1
anku <ankuofficial909@gmail.com>
2024-04-06 22:08:56 +0530

    fixed api mobile issue

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
-const axios = require("axios");
+const apiData = require("./data/apiData.json");
 
 const app = express();
 // Allow requests from anywhere
 app.use(cors());
 // Proxy route
-app.get("/api/v1", async (req, res) => {
-  try {
-    const { data } = await axios.get(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6542&lng=77.2373&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
-      {
-        params: req.query, // Forward query parameters to Swiggy API
-        headers: {
-          "User-Agent": req.get("User-Agent"), // Forward user agent header
-          Referer: req.get("Referer"), // Forward referer header
-        },
-      }
-    );
-    res.json(data);
-  } catch (error) {
-    console.error("Error fetching data from Swiggy API:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
+app.get("/api/v1", (req, res) => {
+  res.json(apiData);
 });
 
 app.get("/", (req, res) => {
