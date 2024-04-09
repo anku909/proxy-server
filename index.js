23cdf39f7df5325b8e9c2164bd4a413cc2ed398e
anku <ankuofficial909@gmail.com>
2024-04-09 21:02:37 +0530

    reupload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const apiData = require("./data/apiData.json");
 const app = express();
 // Allow requests from anywhere
 app.use(cors());
+
 // Proxy route
 app.get("/api/v1", (req, res) => {
   res.json(apiData);
@@ -15,7 +16,7 @@ app.get("/", (req, res) => {
   res.send("Server Running use /api/v1 to get json data");
 });
 
-// Start the server
+// Server Starts
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
